af794502ecc7a3b4b604efff1bb92fce1dabc6d4
Bill-GD <geometrydashbill@gmail.com>
2024-05-07 18:31:38 +0700

    test no routing to /api
    similar to vercel-php example

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php
-header("Location: /api/index.php");
+// route local php server to /api/index.php
+// /api/index.php is for vercel deployment
+// header("Location: /api/index.php");
+echo "Hello Remote World";
